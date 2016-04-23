bde78b0da8c85a528526bfa85cd51c98742bf960
Pamela Yang <ypyang237@gmail.com>
2016-04-22 16:29:03 -1000

    sets up all routes with methods Hello Worlding

--- routes/gallery.js
+++ routes/gallery.js
@@ -3,10 +3,42 @@ const express = require('express'),
       router  = express.Router()
       ;
 
- router.route('/')
+router.route('/')
   .get(function(req, res) {
-    console.log(req.body);
+    console.log('GET', req.body);
     res.json({ success: true });
+  })
+  .post(function(req, res) {
+    console.log('POST', req.body);
+    res.json({success: true});
+  });
+
+router.route('/new')
+  .get(function(req, res) {
+    console.log('GET', req.body);
+    res.json({success: true});
+  });
+
+router.route('/:id')
+  .get(function(req, res) {
+    console.log('GET', req.params.id);
+    res.json({success: true});
+  })
+  .put(function(req, res) {
+    console.log('PUT', req.body);
+    console.log('PUTParams', req.params.id);
+    res.json({success: true});
+  })
+  .delete(function(req, res) {
+    console.log('DELETE', req.body);
+    console.log('DELETEparams', req.params.id);
+    res.json({success: true});
+  });
+
+router.route('/:id/edit')
+  .get(function(req, res) {
+    console.log('GET', req.body);
+    res.json({success: true});
   });
 
 module.exports = router;
