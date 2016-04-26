4b5f0b9e25f4bbaa9c0d51436f6b9f519164e4d4
Owen <owenyangg@gmail.com>
2016-04-26 13:42:42 -1000

    gulp working with nodemon but not browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,53 @@
+'use strict';
+
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var nodemon     = require('gulp-nodemon');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve',['sass'], function() {
 
   browserSync.init({
-      server: "./public"
+      server: "./public",
+      port: 3001
   });
 
   gulp.watch("scss/**/*.scss", ['sass']);
   gulp.watch("public/*.html").on('change', browserSync.reload);
+
+  return gulp.src("scss/styles.scss")
+  .pipe(sass())
+  .pipe(gulp.dest("public/css"))
+  .pipe(browserSync.stream());
+
 });
+
+gulp.task('nodemon', function (cb) {
+
+  var started = false;
+
+  return nodemon({
+    script: 'server.js'
+  }).on('start', function () {
+    // to avoid nodemon being started multiple times
+    // thanks @matthisk
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
+});
+
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
+
   return gulp.src("scss/styles.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("public/css"))
-    .pipe(browserSync.stream());
+  .pipe(sass())
+  .pipe(gulp.dest("public/css"))
+  .pipe(browserSync.stream());
+
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['nodemon', 'serve']);
